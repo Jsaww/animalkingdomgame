bd0d512b93499a771cf5d5fd476b643e707cfcf0
Jsaww <sawyerrjustin@gmail.com>
2022-07-22 11:43:09 -0700

    First Commit

---
+++ js/app.js
@@ -0,0 +1,61 @@
+//Pseudo Code
+// What are your const?
+const animalNames = [
+    "Bear",
+    "Whale",
+    "Zebra",
+    "Scorpion",
+    "Lion",
+    "Horse",
+    "Elephant",
+    "Camel",
+    "Panda",
+    "Hippo",
+    "Rhino",
+    "Koala",
+    "Monkey",
+    "penguin",
+    "Bat",
+    "Raccoon",
+    "Frog"
+    ];
+    
+    
+    
+    // What are your state variables
+    let score;
+    let attemptsLeft;
+    let guessAnswer;
+    let correctAnswer;
+    
+    
+    
+    
+    // What are your cached elements
+    Keeping track of how many answers are correct 
+    keeping track of how many answers are incorrect
+    const inputKeyboardBtns = document.querySelector('#')
+    
+    
+    
+    // What are your Eventlisteners?
+    inputKeyboardBtns.addEventListener('click', animalNameGuess);
+    startMenuBtn.addEventListener('click', Start Menu);
+    
+    
+    
+    // What are your functions?
+    function selectRandomWord(){
+    
+    }
+    
+    function animalRiddle(){
+    
+    }
+    
+    function resetGame(){
+    
+    }
+    function render(){
+    
+    }
